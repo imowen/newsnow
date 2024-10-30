299fb27c79007494cc1763d6b18ef47e06f6306b
Owen <31156070+imowen@users.noreply.github.com>
2024-10-30 17:07:44 +0700

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,10 +3,8 @@ export function Footer() {
     <>
       <a href={`${Homepage}/LICENCE`} target="_blank">MIT LICENCE</a>
       <span>
-        <span>NewsNow © 2024 By </span>
-        <a href={Author.url} target="_blank">
-          {Author.name}
-        </a>
+        <span>NewsNow © 2024 </span>
+        
       </span>
     </>
   )
